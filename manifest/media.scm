754438a5c0f217af227d2f8a9055186b53af68ea
Lu Hui <luhux76@gmail.com>
2023-06-22 18:50:49 +0800

    home: background set to 0x7c7c7c

--- manifest/media.scm
+++ manifest/media.scm
@@ -1,8 +1,8 @@
 (use-modules (gnu) (guix profiles) (hui packages telegram))
-(use-package-modules image-viewers xdisorg video linux kde-multimedia)
+(use-package-modules image-viewers xdisorg video linux kde-multimedia image)
 
 (define-public %media-packages
   (list
-   feh scrot ffmpeg alsa-utils tgs2png k3b))
+   feh swaybg scrot grim ffmpeg alsa-utils tgs2png k3b))
 
 (packages->manifest %media-packages)
