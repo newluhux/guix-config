cd05f739ae657123c6af85ca4c5bd9219dac02a1
Lu Hui <luhux76@gmail.com>
2023-06-22 22:05:56 +0800

    manifest/graphic.scm: enable xwayland

--- manifest/graphic.scm
+++ manifest/graphic.scm
@@ -1,10 +1,10 @@
-(use-modules (gnu) (guix profiles))
+(use-modules (gnu) (guix profiles) (hui packages wm))
 (use-package-modules xorg wm xdisorg qt kde-frameworks terminals)
 
 
 (define-public %graphic-packages
   (list
-   dwl foot bemenu wl-clipboard
+   dwl-hui foot bemenu wl-clipboard
    xset xsetroot xkbset xrdb xkill xclip
    qt5ct breeze-icons))
 
