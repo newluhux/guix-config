aa1467f7f90096dd1a655052956d04dd693e7671
Lu Hui <luhux76@gmail.com>
2023-06-23 18:40:44 +0800

    manifest: remove mipsel-toolchain.scm

--- manifest/mipsel-toolchain.scm
+++
@@ -1,9 +0,0 @@
-(use-modules (guix profiles) (hui packages embedded))
-
-(define-public %mipsel-toolchain
-  (list
-   gcc-mipsel-elf-toolchain
-   gdb-mipsel-elf))
-
-(packages->manifest
- %mipsel-toolchain)
