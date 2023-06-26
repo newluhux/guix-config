ed07b1e2f3eddeb6e8e856cc75b3923250c271a3
Lu Hui <luhux76@gmail.com>
2023-06-26 21:25:05 +0800

    manifest/connect.scm: add usbip-utils

--- manifest/connect.scm
+++ manifest/connect.scm
@@ -1,9 +1,10 @@
 (use-modules (gnu) (guix profiles) (hui packages embedded))
-(use-package-modules ssh terminals admin screen tmux networking virtualization)
+(use-package-modules ssh terminals admin screen tmux networking virtualization
+                     linux)
 
 (define-public %connect-packages
   (list
    openssh picocom lrzsz ek eksw screen tmux gkermit ukermit ckermit iperf
-   putty virt-manager))
+   putty virt-manager usbip-utils))
 
 (packages->manifest %connect-packages)
