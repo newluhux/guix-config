d185c64f7497b935e67f041c6c94b494d32586d6
Lu Hui <luhux76@gmail.com>
2023-06-28 19:45:03 +0800

    manifest/game.scm: add emulator for games

--- manifest/game.scm
+++ manifest/game.scm
@@ -1,7 +1,8 @@
 (use-modules (gnu) (guix profiles))
-(use-package-modules games minetest)
+(use-package-modules games minetest emulators)
 
 (define-public %game-packages
-  (list warzone2100 cataclysm-dda nethack curseofwar minetest))
+  (list warzone2100 cataclysm-dda nethack curseofwar minetest
+        nestopia-ue mgba ppsspp))
 
 (packages->manifest %game-packages)
