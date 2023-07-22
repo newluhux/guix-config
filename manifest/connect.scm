b01d8ed986f850faad4ff1bcd37f25635ddeec8b
Lu Hui <luhux76@gmail.com>
2023-07-22 10:22:58 +0800

    manifest/connect.scm: I download file use bittorrent

--- manifest/connect.scm
+++ manifest/connect.scm
@@ -1,10 +1,10 @@
 (use-modules (gnu) (guix profiles) (hui packages embedded))
 (use-package-modules ssh terminals admin screen tmux networking virtualization
-                     linux)
+                     linux bittorrent)
 
 (define-public %connect-packages
   (list
    openssh picocom lrzsz ek eksw screen tmux iperf ckermit gkermit ukermit
-   putty virt-manager usbip-utils bluez blueman))
+   putty virt-manager usbip-utils bluez blueman qbittorrent))
 
 (packages->manifest %connect-packages)
