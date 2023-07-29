1301fb5e4dca83428eec121550574edb2210f141
Lu Hui <luhux76@gmail.com>
2023-07-29 22:24:00 +0800

    manifest: remove xfe

--- manifest/graphic.scm
+++ manifest/graphic.scm
@@ -5,6 +5,6 @@
 (define-public %graphic-packages
   (list
    cwm vte xset xsetroot xkbset xrdb xkill xclip xhost
-   qt5ct breeze-icons xfe))
+   qt5ct breeze-icons))
 
 (packages->manifest %graphic-packages)
