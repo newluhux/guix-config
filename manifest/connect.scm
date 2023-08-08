5a33b6486af05c1660ad686f89def436635ba6a0
Lu Hui <luhux76@gmail.com>
2023-08-09 07:36:56 +0800

    manifest: add minicom

--- manifest/connect.scm
+++ manifest/connect.scm
@@ -1,10 +1,10 @@
 (use-modules (gnu) (guix profiles) (hui packages embedded))
 (use-package-modules ssh terminals admin screen tmux networking virtualization
-                     linux bittorrent)
+                     linux bittorrent engineering)
 
 (define-public %connect-packages
   (list
    openssh picocom lrzsz ek eksw screen tmux iperf ckermit gkermit ukermit
-   putty virt-manager usbip-utils bluez qbittorrent))
+   putty virt-manager usbip-utils bluez qbittorrent minicom))
 
 (packages->manifest %connect-packages)
