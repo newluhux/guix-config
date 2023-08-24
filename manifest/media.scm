401afca924d8520cfabe81bbe1b1b46755306644
Lu Hui <luhux76@gmail.com>
2023-08-24 19:44:52 +0800

    manifest: add cmus

--- manifest/media.scm
+++ manifest/media.scm
@@ -1,10 +1,10 @@
 (use-modules (gnu) (guix profiles) (hui packages telegram))
 (use-package-modules
- image-viewers xdisorg video linux kde-multimedia image wm pdf)
+ image-viewers xdisorg video linux kde-multimedia image wm pdf music)
 
 (define-public %media-packages
   (list
    feh swaybg scrot grim ffmpeg alsa-utils tgs2png k3b
-   mupdf zathura zathura-pdf-mupdf))
+   mupdf zathura zathura-pdf-mupdf cmus))
 
 (packages->manifest %media-packages)
