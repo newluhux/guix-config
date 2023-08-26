b1efde2b9bcb00543742d1951387409476d3eda8
Lu Hui <luhux76@gmail.com>
2023-08-26 17:37:15 +0800

    manifest: add desktop notify tool

--- manifest/graphic.scm
+++ manifest/graphic.scm
@@ -1,10 +1,10 @@
 (use-modules (gnu) (guix profiles) (hui packages wm))
-(use-package-modules xorg wm xdisorg qt kde-frameworks terminals disk kde-systemtools gnome ocr)
+(use-package-modules xorg wm xdisorg qt kde-frameworks terminals disk kde-systemtools gnome ocr dunst)
 
 
 (define-public %graphic-packages
   (list
    cwm vte sakura xset xsetroot xkbset xrdb xkill xclip xhost
-   qt5ct breeze-icons tesseract-ocr setxkbmap))
+   qt5ct breeze-icons tesseract-ocr setxkbmap libnotify dunst))
 
 (packages->manifest %graphic-packages)
