4768bf87f32f028e44ec3c30b24b45b82fef87f4
Lu Hui <luhux76@gmail.com>
2023-08-26 17:00:39 +0800

    manifest: add tesseract-ocr

--- manifest/graphic.scm
+++ manifest/graphic.scm
@@ -1,10 +1,10 @@
 (use-modules (gnu) (guix profiles) (hui packages wm))
-(use-package-modules xorg wm xdisorg qt kde-frameworks terminals disk kde-systemtools gnome)
+(use-package-modules xorg wm xdisorg qt kde-frameworks terminals disk kde-systemtools gnome ocr)
 
 
 (define-public %graphic-packages
   (list
    cwm vte sakura xset xsetroot xkbset xrdb xkill xclip xhost
-   qt5ct breeze-icons))
+   qt5ct breeze-icons tesseract-ocr))
 
 (packages->manifest %graphic-packages)
