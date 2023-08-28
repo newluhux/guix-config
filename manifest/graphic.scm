f2336dd1f2de4f202acbb0ca7a8207d94ee6a246
Lu Hui <luhux76@gmail.com>
2023-08-28 13:05:01 +0800

    home: add fcitx5

--- manifest/graphic.scm
+++ manifest/graphic.scm
@@ -1,11 +1,12 @@
 (use-modules (gnu) (guix profiles) (hui packages wm) (hui packages wallpaper))
-(use-package-modules xorg wm xdisorg qt kde-frameworks terminals disk kde-systemtools gnome ocr dunst conky)
+(use-package-modules xorg wm xdisorg qt kde-frameworks terminals disk kde-systemtools gnome ocr dunst conky fcitx5)
 
 
 (define-public %graphic-packages
   (list
    cwm vte sakura xset xsetroot xkbset xrdb xkill xclip xhost
    qt5ct breeze-icons tesseract-ocr setxkbmap libnotify dunst
-   freedom-wallpaper conky))
+   freedom-wallpaper conky fcitx5 fcitx5-rime fcitx5-gtk fcitx5-qt
+   fcitx5-gtk4 fcitx5-configtool))
 
 (packages->manifest %graphic-packages)
